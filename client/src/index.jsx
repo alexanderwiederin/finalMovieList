c988bd5f520d01965e63c372ff1d7c81b6b05bc3
alexanderwiederin <alexander.wiederin@gmail.com>
2018-10-21 14:20:25 -0700

    got react working

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,1 +1,21 @@
-index.jsx
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+
+  }
+
+  render() {
+    return (
+      <div className="main">
+      <h1>this works</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
