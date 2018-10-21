c05c23585c16bbdb27487ca60c3220d5a238a1dc
alexanderwiederin <alexander.wiederin@gmail.com>
2018-10-21 13:58:36 -0700

    setup working server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,17 @@
-index.js
+var express = require('express');
+var path = require('path');
+var parseBody = require('body-parser');
+
+var app = express();
+var port = 3000;
+
+console.log(path.join(__dirname, '../client/dist'));
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+
+
+app.listen(port, () => {
+  console.log('listening on port ' + port);
+})
+
